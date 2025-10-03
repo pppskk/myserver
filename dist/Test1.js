cf94289989c9b5f156cb30d2fcd87f6714cdce08
pppskk <namthip_m@cmu.ac.th>
2025-10-03 09:57:57 +0700

    Refactor unit_test to use async/await and clean up commented code

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,16 +1,21 @@
-// "use strict";
-// import { add } from "./Utils";
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const Utils = require("./Utils");
-
-const unit_test = async () => {
-    
+const unit_test = () => __awaiter(void 0, void 0, void 0, function* () {
     if (Utils.add(2, 3) === 5) {
         console.log(0);
     }
     else {
         console.log(1);
     }
-
     if (Utils.add(2, 4) === 7) {
         console.log(0);
         // helloworld();
@@ -18,13 +23,5 @@ const unit_test = async () => {
     else {
         console.log(2);
     }
-}
-
-// if (Utils.add(2, 3) === 5) {
-//     console.log(0);
-//     // helloworld();
-// }
-// else {
-//     console.log(1);
-// }
+});
 unit_test();
